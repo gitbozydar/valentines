858b7f2d67df40b3a01526698576b5e669785751
Ivo Delev <ivo.delev@wp.pl>
2024-02-14 14:03:03 +0100

    da

--- script.js
+++ script.js
@@ -8,41 +8,63 @@ newWrapper.style.display = "flex";
 newWrapper.style.flexDirection = "column";
 const noBtn = document.getElementById("no-btn");
 
-const createAudio = document.createElement("audio");
-const createSource = document.createElement("source");
-createSource.src = "benzyna.wav";
-createSource.type = "audio/wav";
+const ahaAudioShort = new Audio("ahashort.wav");
+const ahaAudioLong = new Audio("ahalong.wav");
+const kissAudio = new Audio("kissyes.wav");
+const benzynaAudio = new Audio("benzyna.wav");
+
 const jsConfetti = new JSConfetti();
 
 yesBtn.addEventListener("click", () => {
   wrapper.remove();
-
   mainContainer.appendChild(newWrapper);
   createCanvas();
   jsConfetti.addConfetti({
     emojis: ["😍", "❤️‍🔥", "❤️"],
   });
-
+  kissAudio.play();
+  benzynaAudio.play();
   const iFrame = document.createElement("iframe");
   iFrame.src = "https://giphy.com/embed/Wcil7wEpUX0CJVX3q9";
   iFrame.style.border = "none";
   buildHappyHeader();
   newWrapper.appendChild(iFrame);
-  newWrapper.appendChild(createAudio);
-  createAudio.appendChild(createSource);
-  createAudio.play();
 });
-
-// noBtn.addEventListener("mouseover", () => {
-//   noBtn.disabled = true;
-// });
-
-// noBtn.addEventListener("mouseout", () => {
-//   noBtn.disabled = false;
-// });
 
 noBtn.addEventListener("click", () => {
   handelSize();
+});
+
+let scaleOfYesBtn = 1;
+let scaleOfNoBtn = 1;
+let counter = 0;
+
+const handelSize = () => {
+  scaleOfYesBtn += 0.1;
+  scaleOfNoBtn -= 0.1;
+  console.log("bok");
+  yesBtn.style.transform = `scale(${scaleOfYesBtn})`;
+  noBtn.style.transform = `scale(${scaleOfNoBtn})`;
+  if (counter === 0) {
+    noBtn.innerHTML = "🥺aha?🥺";
+    ahaAudioShort.play();
+    counter += 1;
+  } else if (counter === 1) {
+    noBtn.innerHTML = "AAAHAA, ok to pa tera😣";
+    ahaAudioLong.play();
+    counter += 1;
+  } else if (counter === 2) {
+    yesBtn.innerHTML = "Mam cię 😈";
+    noBtn.remove();
+  }
+};
+
+benzynaAudio.addEventListener("ended", () => {
+  $(newWrapper).fadeOut();
+  setTimeout(() => {
+    clearNewWrapper();
+    createHeart();
+  }, 1000);
 });
 
 const createCanvas = () => {
@@ -59,23 +81,23 @@ const buildHappyHeader = () => {
   headerOfHappy.innerHTML = "Yaaaaaaay!1!1!❤️‍🔥❤️";
   happyHeader.appendChild(headerOfHappy);
 };
-let scaleOfYesBtn = 1;
-let scaleOfNoBtn = 1;
-let counter = 0;
-const handelSize = () => {
-  scaleOfYesBtn += 0.1;
-  scaleOfNoBtn -= 0.1;
-  console.log("bok");
-  yesBtn.style.transform = `scale(${scaleOfYesBtn})`;
-  noBtn.style.transform = `scale(${scaleOfNoBtn})`;
-  if (counter === 0) {
-    noBtn.innerHTML = "🥺aha?🥺";
-    counter += 1;
-  } else if (counter === 1) {
-    noBtn.innerHTML = "AAAHAA, ok to pa tera😣";
-    counter += 1;
-  } else if (counter === 2) {
-    yesBtn.innerHTML = "Mam cię 😈";
-    noBtn.remove();
-  }
+
+const clearNewWrapper = () => {
+  newWrapper.innerHTML = "";
+};
+
+const createHeart = () => {
+  newWrapper.innerHTML = "❤️";
+  newWrapper.style.fontSize = "10rem";
+  newWrapper.style.animation = "none";
+  $(newWrapper).css("opacity", "0").css("transform", "scale(1)");
+  $(newWrapper).fadeIn();
+  $(newWrapper).animate({
+    opacity: 1,
+    scale: "1.2",
+  });
+  $(newWrapper).animate({
+    opacity: 1,
+    scale: "1",
+  });
 };
